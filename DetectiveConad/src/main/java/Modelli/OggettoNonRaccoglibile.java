1fe7dce5adc7f5695e2594ed5557e611a2c23e61
Vito Musco <v.musco1@studenti.uniba.it>
2021-05-06 18:24:40 +0200

    Aggiunti oggetti non raccoglibili

--- DetectiveConad/src/main/java/Modelli/OggettoNonRaccoglibile.java
+++ DetectiveConad/src/main/java/Modelli/OggettoNonRaccoglibile.java
@@ -1,6 +1,11 @@
 package Modelli;
 
 public class OggettoNonRaccoglibile extends Oggetto{
+    
+    public OggettoNonRaccoglibile(String a,String n){
+        super(a,n);
+    }
+    
     @Override
     public String osservaOggetto(){
         return super.osservaOggetto()+", ma non posso raccoglierlo";
