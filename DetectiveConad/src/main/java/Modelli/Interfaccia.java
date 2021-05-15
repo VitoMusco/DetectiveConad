9c7cc68d11dfe828dfb83c6c4c93234b356bf336
Vito Musco <v.musco1@studenti.uniba.it>
2021-05-15 13:19:51 +0200

    Modifiche interfaccia

--- DetectiveConad/src/main/java/Modelli/Interfaccia.java
+++ DetectiveConad/src/main/java/Modelli/Interfaccia.java
@@ -1,11 +1,20 @@
 package Modelli;
 
+import java.io.File;
+import java.io.IOException;
+import javax.sound.sampled.*;
+
+import java.util.concurrent.TimeUnit;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.*;
+
 public class Interfaccia extends javax.swing.JFrame {
-    
+
     public Interfaccia() {
         initComponents();
     }
-    
+
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
@@ -23,15 +32,21 @@ public class Interfaccia extends javax.swing.JFrame {
         jPanel1.setBackground(new java.awt.Color(0, 0, 0));
         jPanel1.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
-        jButton2.setText("ESCI");
-        jButton2.addMouseListener(new java.awt.event.MouseAdapter() {
-            public void mouseClicked(java.awt.event.MouseEvent evt) {
-                jButton2MouseClicked(evt);
+        jButton2.setBorder(null);
+        jButton2.setBorderPainted(false);
+        jButton2.setContentAreaFilled(false);
+        jButton2.setEnabled(false);
+        jButton2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton2ActionPerformed(evt);
             }
         });
         jPanel1.add(jButton2, new org.netbeans.lib.awtextra.AbsoluteConstraints(150, 480, 150, 40));
 
-        jButton3.setText("NUOVA PARTITA");
+        jButton3.setBorder(null);
+        jButton3.setBorderPainted(false);
+        jButton3.setContentAreaFilled(false);
+        jButton3.setEnabled(false);
         jButton3.addMouseListener(new java.awt.event.MouseAdapter() {
             public void mouseClicked(java.awt.event.MouseEvent evt) {
                 jButton3MouseClicked(evt);
@@ -39,10 +54,11 @@ public class Interfaccia extends javax.swing.JFrame {
         });
         jPanel1.add(jButton3, new org.netbeans.lib.awtextra.AbsoluteConstraints(150, 360, 150, 40));
 
-        jButton4.setText("CARICA PARTITA");
+        jButton4.setBorder(null);
+        jButton4.setBorderPainted(false);
+        jButton4.setContentAreaFilled(false);
+        jButton4.setEnabled(false);
         jPanel1.add(jButton4, new org.netbeans.lib.awtextra.AbsoluteConstraints(150, 420, 150, 40));
-
-        jLabel1.setIcon(new javax.swing.ImageIcon("./risorse/immagini/menu.png"));
         jPanel1.add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 1280, 720));
 
         getContentPane().add(jPanel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 1280, 720));
@@ -50,22 +66,27 @@ public class Interfaccia extends javax.swing.JFrame {
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void jButton2MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jButton2MouseClicked
-        System.exit(0);
-    }//GEN-LAST:event_jButton2MouseClicked
-
     private void jButton3MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jButton3MouseClicked
-        
+
     }//GEN-LAST:event_jButton3MouseClicked
+
+    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
+        System.exit(0);
+    }//GEN-LAST:event_jButton2ActionPerformed
 
     public static void main(String args[]) {
         Mappa mappa = new Mappa();
-        
+        Interfaccia interfaccia = new Interfaccia();
+
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new Interfaccia().setVisible(true);
+                interfaccia.setVisible(true);
             }
         });
+        interfaccia.riproduciAudioIntro();
+        interfaccia.riproduciIntro();
+        interfaccia.riproduciAudioMenu();
+        interfaccia.impostaMenuIniziale();
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
@@ -75,4 +96,74 @@ public class Interfaccia extends javax.swing.JFrame {
     private javax.swing.JLabel jLabel1;
     private javax.swing.JPanel jPanel1;
     // End of variables declaration//GEN-END:variables
+
+    public void riproduciAudioIntro() {
+        File fileAudio = new File("./risorse/audio/intro.wav");
+        AudioInputStream audioStream;
+        try {
+            audioStream = AudioSystem.getAudioInputStream(fileAudio);
+            Clip clip = AudioSystem.getClip();
+            clip.open(audioStream);
+            clip.start();
+        } catch (UnsupportedAudioFileException ex) {
+            Logger.getLogger(Interfaccia.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (LineUnavailableException ex) {
+            Logger.getLogger(Interfaccia.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(Interfaccia.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    public void riproduciIntro() {
+        String prefisso = "";
+        for (int i = 0; i <= 99; i++) {
+            if (i < 10) {
+                prefisso = "000";
+            } else if (i < 100) {
+                prefisso = "00";
+            }
+            jLabel1.setIcon(new ImageIcon("./risorse/immagini/intro/image" + prefisso + i + ".jpg"));
+            try {
+                TimeUnit.MILLISECONDS.sleep(24);
+            } catch (InterruptedException ex) {
+                Logger.getLogger(Interfaccia.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+    }
+    
+    public void riproduciAudioMenu(){
+        File fileAudio = new File("./risorse/audio/menu.wav");
+        AudioInputStream audioStream;
+        try {
+            audioStream = AudioSystem.getAudioInputStream(fileAudio);
+            Clip clip = AudioSystem.getClip();
+            clip.open(audioStream);
+            clip.start();
+        } catch (UnsupportedAudioFileException ex) {
+            Logger.getLogger(Interfaccia.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (LineUnavailableException ex) {
+            Logger.getLogger(Interfaccia.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(Interfaccia.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    public void impostaMenuIniziale() {
+        jButton2.setOpaque(true);
+        jButton2.setContentAreaFilled(true);
+        jButton2.setBorderPainted(true);
+        jButton2.setEnabled(true);
+        jButton2.setText("ESCI");
+        jButton3.setOpaque(true);
+        jButton3.setContentAreaFilled(true);
+        jButton3.setBorderPainted(true);
+        jButton3.setEnabled(true);
+        jButton3.setText("NUOVA PARTITA");
+        jButton4.setOpaque(true);
+        jButton4.setContentAreaFilled(true);
+        jButton4.setBorderPainted(true);
+        jButton4.setEnabled(true);
+        jButton4.setText("CARICA PARTITA");
+        jLabel1.setIcon(new ImageIcon("./risorse/immagini/menu/menu.png"));
+    }
 }
