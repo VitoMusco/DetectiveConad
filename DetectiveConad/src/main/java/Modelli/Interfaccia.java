2a03391cc6695f222ffe231377a5fad5bcea85dc
Vito Musco <v.musco1@studenti.uniba.it>
2021-06-15 10:40:22 +0200

    Modifiche generali

--- DetectiveConad/src/main/java/Modelli/Interfaccia.java
+++ DetectiveConad/src/main/java/Modelli/Interfaccia.java
@@ -18,7 +18,7 @@ import java.util.ListIterator;
 public class Interfaccia extends javax.swing.JFrame {
 
     private final int MAX_PARTITE_SALVATE = 4;
-    private final ImageIcon menu, selettorePartita;
+    private final ImageIcon menu, selettorePartita, finaleCorretto;
     private boolean creazioneNuovaPartita = false;
     private boolean saltaFilmatoIntroduzione = false;
     private boolean riproduzioneFinaleCorretto = false;
@@ -40,6 +40,7 @@ public class Interfaccia extends javax.swing.JFrame {
 
         menu = new ImageIcon("./risorse/immagini/menu/menu.png");
         selettorePartita = new ImageIcon("./risorse/immagini/menu/salvataggi.png");
+        finaleCorretto = new ImageIcon("./risorse/gif/videofinale.gif");
 
         disattivaInterfacciaMenu();
         disattivaInterfacciaUtente();
@@ -908,7 +909,7 @@ public class Interfaccia extends javax.swing.JFrame {
     public void riproduciFinaleCorretto() {
         stoppaAudio();
         disattivaInterfacciaUtente();
-        MediaLabel.setIcon(new ImageIcon("./risorse/gif/videofinale.gif"));
+        MediaLabel.setIcon(finaleCorretto);
         riproduciAudio("videofinale");
         try {
             TimeUnit.SECONDS.sleep(44);
